9c9290c10750216eb06e951dc79056931e381c22
omjinLTS <omjin7g@gmail.com>
2024-12-09 20:28:14 +0900

    video

--- app/screens/CamScreen.tsx
+++ app/screens/CamScreen.tsx
@@ -1,20 +1,24 @@
 import { Screen, Text } from "@/components"
 import { AppStackScreenProps } from "@/navigators"
 import { observer } from "mobx-react-lite"
-import { FC } from "react"
+import { FC, useState } from "react"
 import { ViewStyle } from "react-native"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "@/models"
 import { $styles } from "@/theme"
+import WebView from "react-native-webview"
 
 interface CamScreenProps extends AppStackScreenProps<"Cam"> {}
 
 export const CamScreen: FC<CamScreenProps> = observer(function CamScreen() {
+  const [loading, setLoading] = useState(true);
+
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+
   return (
     <Screen
       style={$root}
@@ -22,7 +26,20 @@ export const CamScreen: FC<CamScreenProps> = observer(function CamScreen() {
       safeAreaEdges={["top"]}
       contentContainerStyle={$styles.container}
     >
-      <Text text="cam" />
+      <Text text="cam" style={{
+        marginBottom: 100
+      }}/>
+      {loading && <Text text="Loading..." />}
+      <WebView
+      source={{ uri: "http://59.187.251.226:24549/janus"}}
+      onLoad={() => setLoading(false)}
+      onError={(e) => console.log(e)}
+      originWhitelist={["https://*", "http://*", "file://*"]}
+      style={{
+        width: "100%",
+        height: 250,
+        flex: 1,
+      }}/>
     </Screen>
   )
 })
--- app/screens/MainScreen.tsx
+++ app/screens/MainScreen.tsx
@@ -2,6 +2,7 @@ import { Checkbox, Screen, Text, TowelProgressBar } from "@/components"
 import { AppStackScreenProps } from "@/navigators"
 import { ThemedStyle } from "@/theme"
 import { useAppTheme } from "@/utils/useAppTheme"
+import useRealtimeData from "@/utils/useRealtimeData"
 import { observer } from "mobx-react-lite"
 import { FC } from "react"
 import { TextStyle, View, ViewStyle } from "react-native"
@@ -10,8 +11,22 @@ import { TextStyle, View, ViewStyle } from "react-native"
 
 interface MainScreenProps extends AppStackScreenProps<"Main"> {}
 
+interface DangerData {
+  key: number
+}
+
 export const MainScreen: FC<MainScreenProps> = observer(function MainScreen() {
   const { themed } = useAppTheme()
+  const {data: indoorDanger, error: indoorDangerError} = useRealtimeData<DangerData>("/indoor/danger")
+  const {data: carDanger, error: carDangerError} = useRealtimeData<DangerData>("/car/danger")
+
+  const checkRecentDanger = (keyTime: number) => {
+    const dangerTime = new Date(keyTime * 1000)
+    const currentTime = new Date()
+    const diff = currentTime.getTime() - dangerTime.getTime()
+    // 5 min
+    return diff < 1000 * 60 * 5
+  }
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
 
@@ -28,11 +43,11 @@ export const MainScreen: FC<MainScreenProps> = observer(function MainScreen() {
       <TowelProgressBar style={$progressBar} />
       <View style={$checkboxContainer}>
         <Text text="위험 상황 여부" style={themed($checkboxText)} />
-        <Checkbox />
+        <Checkbox value={indoorDanger ? checkRecentDanger(indoorDanger.key) : false}/>
       </View>
       <View style={$checkboxContainer}>
         <Text text="차량 열림 여부" style={themed($checkboxText)} />
-        <Checkbox />
+        <Checkbox value={carDanger ? checkRecentDanger(carDanger.key) : false} />
       </View>
     </Screen>
   )
@@ -56,6 +71,7 @@ const $container: ViewStyle = {
 const $title: ThemedStyle<TextStyle> = ({ typography }) => ({
   fontFamily: typography.primary.bold,
   fontSize: 18,
+  marginBottom: 30
 })
 
 const $progressBar: ViewStyle = {
